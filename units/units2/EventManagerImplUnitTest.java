794eb1e4054227176bfcf2a64e0f63762d3de8e6
Tanay Kumar Bera <sri@tanay.co>
2018-10-13 08:39:13 +0530

    New tests in eventmanagerimpl

--- units/units2/EventManagerImplUnitTest.java
+++ units/units2/EventManagerImplUnitTest.java
@@ -44,4 +44,37 @@ public class EventManagerImplUnitTest {
         verify(eventMap, times(1)).put(eq(payload.getFlowID()), any(EventWrapper.class));
         verify(lock, times(1)).unlock();
     }
+
+    @Test
+    public void testPushEventWithNullFlowId() {
+        PaymentInitWrapper payload = new PaymentInitWrapper();
+        payload.setFlowID(null);
+        payload.setParentFlowID(null);
+        // .. continue setting other fields like before
+
+        eventManager.pushEvent(EventStage.SOME_FLOW, payload); // in case of null flow id, nothing should be invoked
+        verify(eventMapTrace, times(0)).put(anyString(), any(EventWrapper.class));
+        verify(lock, times(0)).lock();
+        verify(eventMap, times(0)).put(eq(payload.getFlowID()), any(EventWrapper.class));
+        verify(lock, times(0)).unlock();
+    }
+
+    @Test
+    public void testKafkaEventCollector() {
+        PaymentInitWrapper payload = new PaymentInitWrapper();
+        payload.setFlowID("233243");
+        payload.setParentFlowID("23324332");
+        // .. continue setting other fields like before
+
+        // use reflection to enable kafka
+        Field field = ReflectionUtils.findField(EventManagerImpl.class, "isEnabled");
+        ReflectionUtils.makeAccessible(field);
+        ReflectionUtils.setField(field, eventManager, true);
+
+        eventManager.pushEvent(EventStage.SOME_FLOW, payload);
+
+        // in case isEnabled true, shall go to if block
+        // so check if a call has been made to KafkaTemplate
+        verify(kafkaEventCollectorQueueService, times(1)).send(anyString(), anyString());
+    }
 }
